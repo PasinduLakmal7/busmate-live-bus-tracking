9c6e40e3b62a244b818baab10e4cb4dc2e628089
Pasindu Lakmal <pherath119@gmail.com>
2026-02-06 23:40:23 +0530

    controller make

--- busmate-backend/src/routers/authRouter.js
+++ busmate-backend/src/routers/authRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const registerUser = require('../controllers/registerUser');
+const registerUser = require('../controllers/registerController');
 
 router.post("/login", registerUser);
 router.post("/register", registerUser);
